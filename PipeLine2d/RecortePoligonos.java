e68dca399c49248e7574348dcde4e6de0666cbad
Leoz <Leoz@Leoz-PC>
2012-10-10 10:29:42 -0500

    Recorte de poligonos

---
+++ PipeLine2d/RecortePoligonos.java
@@ -0,0 +1,27 @@
+	import java.util.ArrayList;
+	import processing.core.PApplet;
+	import processing.core.PVector;
+	
+public class RecortePoligonos extends PApplet {
+	
+	Poligono p;
+	ArrayList<PVector> ventana = new ArrayList<PVector>();
+	int delta = 20;
+	
+	public void setup() {
+		size(500, 500);
+		background(0);
+		ventana.add(new PVector(delta , delta));
+		ventana.add(new PVector(delta , height-delta));
+		ventana.add(new PVector(width-delta , height-delta));
+		ventana.add(new PVector(width-delta , delta));
+		recorte(p);
+	}
+	
+	
+	public Poligono recorte(Poligono g){
+		Poligono n = new Poligono();
+		return n;
+	}
+
+}
