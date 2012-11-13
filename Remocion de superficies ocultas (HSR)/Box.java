71ec36965bb3e5361954e32b64673a3f196486f0
Sebastian <Sebastian@Calliope>
2012-11-13 18:20:59 -0500

    Primitive trinangle plane

--- Remocion de superficies ocultas (HSR)/Box.java
+++ Remocion de superficies ocultas (HSR)/Box.java
@@ -1,118 +1,105 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.core.PGraphics;
 import processing.core.PVector;
 import remixlab.proscene.InteractiveFrame;
 import remixlab.proscene.Quaternion;
 import remixlab.proscene.Scene;
 
-/**
- * Box. 
- * by Jean Pierre Charalambos.
- * 
- * This class is part of the Mouse Grabber example.
- *
- * Any object that needs to be "pickable" (such as the Box), should be
- * attached to its own InteractiveFrame. That's all there is to it.
- *
- * The built-in picking proscene mechanism actually works as follows. At
- * instantiation time all InteractiveFrame objects are added to a mouse
- * grabber pool. Scene parses this pool every frame to check if the mouse
- * grabs a InteractiveFrame by projecting its origin onto the screen.
- * If the mouse position is close enough to that projection (default
- * implementation defines a 10x10 pixel square centered at it), the object
- * will be picked. 
- *
- * Override InteractiveFrame.checkIfGrabsMouse if you need a more
- * sophisticated picking mechanism.
- *
- * Observe that this class is used among many examples, such as MouseGrabber
- * CajasOrientadas, PointUnderPixel and ScreenDrawing. Hence, it's quite
- * complete, but its functionality is not totally exploited by this example.
- */
-
 public class Box {
-  InteractiveFrame iFrame;
-  float w, h, d;
-  int c;
-  private PApplet pApplet;
-  private Scene scene;
+	private static ArrayList<PVector> vertices = new ArrayList<PVector>(8);
 
-  Box(Scene scene) {
-    iFrame = new InteractiveFrame(scene);
-    pApplet = scene.parent;
-    this.scene=scene;
-    setSize();
-    setColor();
-    setPosition();
-  }
-  
-  // don't draw local axis
-  public void draw() {
-    draw(false);
-  }
+	InteractiveFrame iFrame;
+	float width, height, depth;
+	int color;
+	private PGraphics canvas;
+	private Scene scene;
 
-  public void draw(boolean drawAxis) {
-    pApplet.pushMatrix();
-    pApplet.pushStyle();
-    // Multiply matrix to get in the frame coordinate system.
-    // scene.parent.applyMatrix(iFrame.matrix()) is handy but inefficient
-    iFrame.applyTransformation(); //optimum
-    if(drawAxis) scene.drawAxis(PApplet.max(w,h,d)*1.3f);
-    pApplet.noStroke();
-    if (iFrame.grabsMouse())
-    	pApplet.fill(255, 0, 0);
-    else
-    	pApplet.fill(getColor());
-    //Draw a box
-    scene.renderer().box(w,h,d);
-    pApplet.popStyle();
-    pApplet.popMatrix();
-  }
-  
-  // sets size randomly
-  public void setSize() {
-    w = pApplet.random(10, 40);
-    h = pApplet.random(10, 40);
-    d = pApplet.random(10, 40);
-  }
-  
-  public void setSize(float myW, float myH, float myD) {
-    w=myW; h=myH; d=myD;
-  }
-  
-  public int getColor() {
-    return c;
-  }
-  
-  // sets color randomly
-  public void setColor() {
-    c = pApplet.color(pApplet.random(0, 255), pApplet.random(0, 255), pApplet.random(0, 255));
-  }
-  
-  public void setColor(int myC) {
-    c = myC;
-  }
-  
-  public PVector getPosition() {
-    return iFrame.position();
-  }
-  
-  // sets position randomly
-  public void setPosition() {
-    float low = -100;
-    float high = 100;
-    iFrame.setPosition(new PVector(pApplet.random(low, high), pApplet.random(low, high), pApplet.random(low, high)));
-  }
-  
-  public void setPosition(PVector pos) {
-    iFrame.setPosition(pos);
-  }
-  
-  public Quaternion getOrientation() {
-    return iFrame.orientation();
-  }
-  
-  public void setOrientation(PVector v) {
-    PVector to = PVector.sub(v, iFrame.position());
-    iFrame.setOrientation(new Quaternion(new PVector(0,1,0), to));
-  }
+	Box(Scene scene) {
+		vertices.clear();
+		vertices.add(new PVector(0, 0, 0));
+		vertices.add(new PVector(0, 1, 0));
+		vertices.add(new PVector(1, 0, 0));
+
+		iFrame = new InteractiveFrame(scene);
+		canvas = scene.renderer();
+		this.scene = scene;
+
+		// sets size randomly
+		width = scene.parent.random(10, 40);
+		height = scene.parent.random(10, 40);
+		depth = scene.parent.random(10, 40);
+
+		// sets color randomly
+		color = scene.parent.color(scene.parent.random(0, 255), scene.parent.random(0, 255),
+				scene.parent.random(0, 255));
+
+		setPosition();
+	}
+
+	public void draw() {
+		canvas.pushMatrix();
+		canvas.pushStyle();
+		// Multiply matrix to get in the frame coordinate system.
+		// scene.parent.applyMatrix(iFrame.matrix()) is handy but inefficient
+		iFrame.applyTransformation(); // optimum
+
+		canvas.noStroke();
+		if (iFrame.grabsMouse()) {
+			canvas.fill(255, 0, 0);
+			scene.drawAxis(PApplet.max(width, height, depth) * 1.3f);
+		} else
+			canvas.fill(getColor());
+		// Draw a box
+		// canvas.box(w, h, d);
+		canvas.stroke(this.getColor());
+		canvas.beginShape();
+		for (PVector vertice : vertices) {
+			canvas.vertex(vertice.x * width, vertice.y * height, vertice.z * depth);
+		}
+		canvas.endShape(PGraphics.CLOSE);
+
+		canvas.popStyle();
+		canvas.popMatrix();
+	}
+
+	public void setSize(float myW, float myH, float myD) {
+		width = myW;
+		height = myH;
+		depth = myD;
+	}
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int myC) {
+		color = myC;
+	}
+
+	public PVector getPosition() {
+		return iFrame.position();
+	}
+
+	// sets position randomly
+	public void setPosition() {
+		float low = -100;
+		float high = 100;
+		iFrame.setPosition(new PVector(scene.parent.random(low, high), scene.parent.random(low,
+				high), scene.parent.random(low, high)));
+	}
+
+	public void setPosition(PVector pos) {
+		iFrame.setPosition(pos);
+	}
+
+	public Quaternion getOrientation() {
+		return iFrame.orientation();
+	}
+
+	public void setOrientation(PVector v) {
+		PVector to = PVector.sub(v, iFrame.position());
+		iFrame.setOrientation(new Quaternion(new PVector(0, 1, 0), to));
+	}
 }
--- Remocion de superficies ocultas (HSR)/PointUnderPixel.java
+++ Remocion de superficies ocultas (HSR)/PointUnderPixel.java
@@ -5,95 +5,81 @@ import processing.opengl.PGraphicsOpenGL;
 import remixlab.proscene.CameraProfile;
 import remixlab.proscene.Scene;
 
-
 @SuppressWarnings("serial")
-public class PointUnderPixel extends PApplet{
-	/**
-	 * Point Under Pixel.
-	 * by Jean Pierre Charalambos.
-	 * 
-	 * This example illustrates 3D world point picking.
-	 * 
-	 * The click bindings below define some per-pixel user interactions,
-	 * such as "zoom on pixel" or "set the arcball reference point". 
-	 * 
-	 * Press 'h' to display the global shortcuts in the console.
-	 * Press 'H' to display the current camera profile keyboard shortcuts
-	 * and mouse bindings in the console.
-	 */
+public class PointUnderPixel extends PApplet {
 
 	Scene scene;
-	Box [] boxes;
+	Box[] boxes;
 	private PGraphics canvas;
 	private PGraphics derecho;
 
 	public void setup() {
-	  size(640, 480, P3D);
-	  hint(DISABLE_DEPTH_TEST);
-	  canvas = createGraphics(width/2, height,P3D);
-	  derecho = createGraphics(width/2, height);
-	  scene = new Scene(this,(PGraphicsOpenGL) canvas);
-	  scene.setShortcut('f', Scene.KeyboardAction.DRAW_FRAME_SELECTION_HINT);
-	  scene.setShortcut('z', Scene.KeyboardAction.ARP_FROM_PIXEL);
-	  //add the click actions to all camera profiles
-	  CameraProfile [] camProfiles = scene.getCameraProfiles();
-	  for (int i=0; i<camProfiles.length; i++) {
-	    // left click will zoom on pixel:
-	    camProfiles[i].setClickBinding( Scene.Button.LEFT, Scene.ClickAction.ZOOM_ON_PIXEL );
-	    // middle click will show all the scene:
-	    camProfiles[i].setClickBinding( Scene.Button.MIDDLE, Scene.ClickAction.SHOW_ALL);
-	    // right click will will set the arcball reference point:
-	    camProfiles[i].setClickBinding( Scene.Button.RIGHT, Scene.ClickAction.ARP_FROM_PIXEL );
-	    // double click with the middle button while pressing SHIFT will reset the arcball reference point:
-	    camProfiles[i].setClickBinding( Scene.Modifier.SHIFT.ID, Scene.Button.MIDDLE, 2, Scene.ClickAction.RESET_ARP );
-	  }	  
+		size(640, 480, P3D);
+		canvas = createGraphics(width / 2, height, P3D);
+		derecho = createGraphics(width / 2, height);
+		scene = new Scene(this, (PGraphicsOpenGL) canvas);
+		scene.setShortcut('f', Scene.KeyboardAction.DRAW_FRAME_SELECTION_HINT);
+		scene.setShortcut('z', Scene.KeyboardAction.ARP_FROM_PIXEL);
+		// add the click actions to all camera profiles
+		CameraProfile[] camProfiles = scene.getCameraProfiles();
+		for (int i = 0; i < camProfiles.length; i++) {
+			// left click will zoom on pixel:
+			camProfiles[i].setClickBinding(Scene.Button.LEFT, Scene.ClickAction.ZOOM_ON_PIXEL);
+			// middle click will show all the scene:
+			camProfiles[i].setClickBinding(Scene.Button.MIDDLE, Scene.ClickAction.SHOW_ALL);
+			// right click will will set the arcball reference point:
+			camProfiles[i].setClickBinding(Scene.Button.RIGHT, Scene.ClickAction.ARP_FROM_PIXEL);
+			// double click with the middle button while pressing SHIFT will
+			// reset the arcball reference point:
+			camProfiles[i].setClickBinding(Scene.Modifier.SHIFT.ID, Scene.Button.MIDDLE, 2,
+					Scene.ClickAction.RESET_ARP);
+		}
 
-	  scene.setGridIsDrawn(false);
-	  scene.setAxisIsDrawn(false);
-	  scene.setRadius(200);
-	  scene.enableFrustumEquationsUpdate();
-	  scene.addDrawHandler(this, "mainDrawing");
-	  scene.showAll();
-	  boxes = new Box[50];
-	  // create an array of boxes with random positions, sizes and colors
-	  for (int i = 0; i < boxes.length; i++)
-	    boxes[i] = new Box(scene);
-	  boxes[0].setPosition(new PVector(width/2, height/2, boxes[0].getPosition().z));
+		scene.setGridIsDrawn(false);
+		scene.setAxisIsDrawn(false);
+		scene.setRadius(200);
+		scene.enableFrustumEquationsUpdate();
+		scene.addDrawHandler(this, "mainDrawing");
+		scene.showAll();
+		boxes = new Box[50];
+		// create an array of boxes with random positions, sizes and colors
+		for (int i = 0; i < boxes.length; i++)
+			boxes[i] = new Box(scene);
+		boxes[0].setPosition(new PVector(width / 2, height / 2, boxes[0].getPosition().z));
 	}
 
-	public void mainDrawing(Scene s){
+	public void mainDrawing(Scene s) {
 		PGraphicsOpenGL p = s.renderer();
 		p.background(0);
-		  for (int i = 0; i < boxes.length; i++){    
-		    boxes[i].draw();
-		  }
+		for (int i = 0; i < boxes.length; i++) {
+			boxes[i].draw();
+		}
 	}
-	
+
 	public void draw() {
-		
-		if(mouseX > width/2){
+
+		if (mouseX > width / 2) {
 			scene.disableMouseHandling();
-		}else{
+		} else {
 			scene.enableMouseHandling();
 		}
-		
-	  canvas.beginDraw();
-	  scene.beginDraw();
-	  scene.endDraw();
-	  canvas.endDraw();
-	  image(canvas, 0, 0);
-	  
-	  System.out.print(boxes[0].getPosition()+"\t");
-	  PVector window = scene.camera().projectedCoordinatesOf(boxes[0].getPosition());
-	  System.out.println(window);
-	  
-	  derecho.beginDraw();
-	  derecho.background(0);
-	  derecho.stroke(255,0,0);
-	  derecho.strokeWeight(5);
-	  derecho.ellipse(window.x, window.y, 5, 5);
-	  derecho.endDraw();
-	  
-	  image(derecho,width/2,0,width,height);
+
+		canvas.beginDraw();
+		canvas.hint(PGraphics.DISABLE_DEPTH_TEST);
+		scene.beginDraw();
+		scene.endDraw();
+		canvas.endDraw();
+		image(canvas, 0, 0);
+
+		System.out.print(boxes[0].getPosition() + "\t");
+		PVector window = scene.camera().projectedCoordinatesOf(boxes[0].getPosition());
+		System.out.println(window);
+		derecho.beginDraw();
+		derecho.background(255);
+		derecho.stroke(255, 0, 0);
+		derecho.ellipse(window.x, window.y, 5, 5);
+		derecho.endDraw();
+
+		image(derecho, width / 2, 0);
 	}
 }
